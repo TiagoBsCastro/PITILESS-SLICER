3a16332f6447b419a8a417f1716a4c73a07ad200
Tiago Castro <tiago@lapoderosa.oats.inaf.it>
2019-03-26 14:29:52 +0100

    Cleaning

--- readplc.py
+++
@@ -1,8 +0,0 @@
-import ReadPinocchio as rp
-import numpy as np
-from astropy.io import fits
-import matplotlib.pyplot as plt
-
-plc = rp.plc("pinocchio.cdm.plc.out")
-cat = rp.catalog("pinocchio.0.0000.cdm.catalog.out")
-hist = rp.histories("pinocchio.cdm.histories.out")
