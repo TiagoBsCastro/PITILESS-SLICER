bc33c868a8949d5046ccad64433bb5de7ba078d7
TiagoBsCastro <tiagobscastro@github.com>
2020-09-10 11:11:03 +0200

    Update: 10/09/2020

--- cosmology.py
+++ cosmology.py
@@ -5,7 +5,7 @@ from astropy.constants import c
 from astropy.cosmology import FlatLambdaCDM, z_at_value
 from astropy.units import Mpc
 
-################# Cosmological Model ##################
+########################### Cosmological Model ##########################
 lcdm  = FlatLambdaCDM(H0=params.h0, Om0=params.omega0)
 cspeed = c.to('km/s').value
 
@@ -13,7 +13,7 @@ zinterp = np.linspace(0, params.zsource + 0.1, 1000)
 ainterp = 1.0/(1.0+zinterp)
 Dinterp = lcdm.comoving_distance(zinterp).value
 
-##################### Lens Planes #####################
+############################# Lens Planes ###############################
 
 lensthickness = params.boxsize/params.nlensperbox
 nreplications = int(lcdm.comoving_distance(params.zsource).value/lensthickness + 1 )
@@ -21,7 +21,7 @@ zl    = [0] + [ z_at_value(lcdm.comoving_distance, n*lensthickness*Mpc, zmax = 1
 zlsup = zl[1:  ]
 zlinf = zl[ :-1]
 
-########## Pinocchio Cosmological Quantities ##########
+################### Pinocchio Cosmological Quantities ###################
 
 (a, t, D, D2, D31, D32) = np.loadtxt(params.pincosmofile, usecols=(0,1,2,3,4,5), unpack=True)
 
