d47f507666ba417725e5fa34b42e97d06ccb9982
TiagoBsCastro <tiagobscastro@github.com>
2020-09-23 16:32:04 +0200

    Dev merged: 23/09/2020

--- plot-LSS.py
+++ plot-LSS.py
@@ -2,19 +2,18 @@ import params
 import matplotlib.pyplot as plt
 import numpy as np
 import cosmology
-from IO.ReadPinocchio import catalog
-from IO import snapshot
+from IO.Pinocchio.ReadPinocchio import catalog
+from IO.Pinocchio.TimelessSnapshot import timeless_snapshot
 
-snap = snapshot.Timeless_Snapshot(params.pintlessfile)
-
-posp = np.transpose(snap.snapPos(0.0, zcentered=False)) + params.boxsize/2
+snap = timeless_snapshot(params.pintlessfile)
+posp = snap.snapPos(0.0, zcentered=False) + params.boxsize/2
 
 cat = catalog("pinocchio.0.0000.example.catalog.out")
 
 cut = posp[:,2] < 20
 plt.scatter(posp[cut, 0], posp[cut, 1], s=0.001)
 plt.figure()
-cut = (cat.pos[:, 2] < 20) & (cat.Mass > 1e14)
+cut = (cat.pos[:, 2] < 20)
 plt.scatter(cat.pos[cut, 0], cat.pos[cut, 1], s=0.1, c='r')
 
 plt.show()
