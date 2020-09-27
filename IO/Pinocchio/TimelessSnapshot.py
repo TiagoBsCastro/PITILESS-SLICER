Bug fix: fixing pos rotation 27/09/2020

IO/Pinocchio/TimelessSnapshot.py
@@ -64,8 +64,16 @@ class timeless_snapshot:
         face = 1
         sgn  = [1, 1, 1]
         # Recentering the box
-        self.qPos = np.array([ (self.ID-1)%self.NG,((self.ID-1)//self.NG)%self.NG,\
-                              ((self.ID-1)//self.NG**2)%self.NG ]).transpose() * self.Cell + self.Cell/2.
+        if params.rotatebox:
+
+            self.qPos = np.array([ ((self.ID-1)//self.NG)%self.NG, ((self.ID-1)//self.NG**2)%self.NG,\
+                                   (self.ID-1)%self.NG ]).transpose() * self.Cell + self.Cell/2.
+
+        else:
+
+            self.qPos = np.array([ (self.ID-1)%self.NG,((self.ID-1)//self.NG)%self.NG,\
+                                  ((self.ID-1)//self.NG**2)%self.NG ]).transpose() * self.Cell + self.Cell/2.
+
         self.qPos = randomizePositions(params.plccenter, face, sgn, self.qPos/self.Lbox)
         self.V1   = self.Cell*randomizeVelocities(face, sgn, self.V1)/self.Lbox
         self.V2   = self.Cell*randomizeVelocities(face, sgn, self.V2)/self.Lbox