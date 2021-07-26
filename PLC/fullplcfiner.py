5cb1d6a41862bab196e3d54d6d9a4026d14b24d5
TiagoBsCastro <tiagobscastro@gmail.com>
2021-07-26 09:13:12 +0200

    Faster concetration computation, 26/07/2021

--- PLC/fullplcfiner.py
+++ PLC/fullplcfiner.py
@@ -11,6 +11,7 @@ import IO.Pinocchio.ReadPinocchio as rp
 import astropy.coordinates as ap
 import NFW.NFWx as NFW
 from time import time
+from scipy.interpolate import interp2d
 
 # Bunch class for easy MPI handling
 class Bunch:
@@ -252,6 +253,12 @@ if not rank:
 
    kappa = np.zeros(params.npixels)
 
+   print("Building the concentration interpolator")
+   m_interp = np.geomspace(0.95 * params.minhalomass*mpart, 1e16, 50)
+   z_interp = np.linspace(0.0, params.zsource, 50)
+   c_interp = np.array( [cosmology.concentration.concentration( m_interp, '200c', z, model = params.cmmodel) for z in z_interp], dtype=np.float32 )
+   c_interp = inter2d( m_interp, z_interp, c_interp, kind='cubic' )
+
    for z1, z2 in zip(cosmology.zlinf, cosmology.zlsup):
 
       print("Lens plane from z=[{0:.3f},{1:.3f}]".format(z1,z2))
@@ -279,7 +286,7 @@ if not rank:
             print(" Computing the concentration")
             rhoc   = cosmology.lcdm.critical_density(plc.redshift[groupsinplane]).to("M_sun/Mpc^3").value/(1+plc.redshift[groupsinplane])**3
             rDelta = np.ascontiguousarray((3*plc.Mass[groupsinplane]/4/np.pi/200/rhoc)**(1.0/3), dtype=np.float32)
-            conc   = np.array( [cosmology.concentration.concentration( m, '200c', z, model = params.cmmodel) for m, z in zip(plc.Mass[groupsinplane], plc.redshift[groupsinplane])], dtype=np.float32 )
+            conc   = c_interp(plc.Mass[groupsinplane], plc.redshift[groupsinplane])
 
             print(" Sampling particle on halos")
             N_part = np.ascontiguousarray( np.round(plc.Mass[groupsinplane]/mpart).astype(np.int32) )
